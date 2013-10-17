72226963ec03b3438c44729c6a75b54a73889d4b
iamblue <tonyone0902@gmail.com>
2013-10-17 16:05:55 +0800

    fixed get,post bug and modify some service

--- ngapi.js
+++ ngapi.js
@@ -11,44 +11,56 @@ angular.module('ngapi',[])
         			routes[i] = data;
         			if(h.test(v)){
         				routes[i].url = 'http://'+v.replace('http://','').split('/')[0]; 
-        			}
-        			// console.log(routes);
+        			};
         		});
         	})
         	return routes;
         };
 	}])
-	.directive('ngApi',['apiroutes','apicrypto','apicnonce','apivalidate','$http',function(apiroutes, apicrypto, apicnonce, apivalidate, $http){
+	.service('apitools',function(){
 		/* validate method part , you can expand it by apivalidate*/
-		var methods = {
-			validate:{
-				email:function(v){
-					var _ckEmail = /.+\@.+\..+/;
-					return _ckEmail.test(v);
-				},
-				number:function(v){
-					var _cknumber = /[0-9]/g;
-					return _cknumber.test(v);	
-				},
-				string:function(v){
-					var _chspec = /\w+/;
-					if(v.match(_chspec)[0] == v){
-						return true;
-					}else{
-						return false;
-					}
-				},
-				nospace:function(v){
-					var _ckspace = /\s/;
-					if(_ckspace.test(v)){
-						return false;
-					}else{
-						return true;
-					}	
+		this.validate = {
+			email:function(v){
+				var _ckEmail = /.+\@.+\..+/;
+				return _ckEmail.test(v);
+			},
+			number:function(v){
+				var _cknumber = /[0-9]/g;
+				return _cknumber.test(v);	
+			},
+			string:function(v){
+				var _chspec = /\w+/;
+				if(v.match(_chspec)[0] == v){
+					return true;
+				}else{
+					return false;
 				}
+			},
+			nospace:function(v){
+				var _ckspace = /\s/;
+				if(_ckspace.test(v)){
+					return false;
+				}else{
+					return true;
+				}	
 			}
-		};
-
+		},
+		/* reg parse method */
+		this.reg = {
+			init:function(v){
+				return v.split(/^\(/)[1].split(/\)$/)[0].split('||');
+			},
+			specColon:function(v){
+				var _v = /:/;
+				if(_v.test(v)){
+					return v.split(/:/);	
+				}else{
+					return false;
+				}
+        	}
+		}
+	})
+	.directive('ngApi',['apiroutes','apicrypto','apicnonce', 'apitools', 'apivalidate', '$http',function(apiroutes, apicrypto, apicnonce, apitools, apivalidate, $http){
 		return {
 			replace: true,
         	restrict: "AE",
@@ -56,44 +68,38 @@ angular.module('ngapi',[])
         		var _this = scope;
         		var _s = attr.ngApi;
         		_s = _s.replace(' ','');
-        		var _d = /(\(.*\))(\(.*\))/;
+        		var _d = /(\(.*\))(\(.*\))/; //()()
+        		var _dd = /(^\(\(|^\(\w+\(.*)/; // (())
         		var _data = _s.match(_d);
-        		/* reg parse method */
-        		var _reg = {
-        			init:function(v){
-        				return v.split(/^\(/)[1].split(/\)$/)[0].split('||');
-        			},
-        			specColon:function(v){
-        				var _v = /:/;
-        				if(_v.test(v)){
-        					return v.split(/:/);	
-        				}else{
-        					return false;
-        				}
-        			}
-        		}
 
         		elem.bind('click',function(){
-        			if(_data.length <=3){
-	        			var _tmpdata = _reg.init(_data[1]);
-	        			var _tmpapi = _reg.init(_data[2]);
+        			if(!_d.test(_data[1]) && !_data[1].match(_dd) && !_data[2].match(_dd)){
+	        			var _tmpdata = apitools.reg.init(_data[1]);
+	        			var _tmpapi = apitools.reg.init(_data[2]);
+	        			if(!!_data[3]){
+	        				var _tmpvalidateerr = apitools.reg.init(_data[3]);	
+	        			}
 	        			var _ckapi = /:/;
 
 	        			angular.forEach(_tmpdata,function(v,i,o){
-	        				if(methods.validate[_reg.specColon(v)[1]]){
-	        					if(!!!methods.validate[_reg.specColon(v)[1]](_this[_reg.specColon(v)[0]])){
-	        						console.log('驗證不通過');
+	        				if(apitools.validate[apitools.reg.specColon(v)[1]]){
+	        					if(!!!apitools.validate[apitools.reg.specColon(v)[1]](_this[apitools.reg.specColon(v)[0]])){
+	        						if(!!_data[3]){
+	        							_this[_tmpvalidateerr](apitools.reg.specColon(v)[0]);
+	        						}
 	        						return false;
 	        					}	
 	        				}else{
-	        					if(!!!apivalidate[_reg.specColon(v)[1]](_this[_reg.specColon(v)[0]])){
-	        						console.log('驗證不通過');
+	        					if(!!!apivalidate[apitools.reg.specColon(v)[1]](_this[apitools.reg.specColon(v)[0]])){
+	        						if(!!_data[3]){
+	        							_this[_tmpvalidateerr](apitools.reg.specColon(v)[0]);
+	        						}
 	        						return false;
 	        					}
 	        				}
 	        			});
 
-	        			var _m = _reg.specColon(_tmpapi[0])[0] || apiroutes[_tmpapi[0].split('.')[0]][_tmpapi[0].replace(_tmpapi[0].split('.')[0]+'.','')].methods[0];
+	        			var _m = apitools.reg.specColon(_tmpapi[0])[0] || apiroutes[_tmpapi[0].split('.')[0]][_tmpapi[0].replace(_tmpapi[0].split('.')[0]+'.','')].methods[0];
 	        			_m = _m.toLowerCase();
 	        			
 	        			var _success = attr.apiSuccess;
@@ -104,7 +110,11 @@ angular.module('ngapi',[])
 	        			switch (_m){
 	        				case 'get':
 	        					if(_ckapi.test(_tmpapi)){
-	        						$http.get(apiroutes+_reg.specColon(_tmpapi[0])[1])
+	        						var _sd = {};
+									angular.forEach(_tmpdata,function(v,i,o){
+										_sd[v.split(':')[0]]=_this[v.split(':')[0]]
+									});
+	        						$http.get(apiroutes+apitools.reg.specColon(_tmpapi[0])[1],{params: _sd})
 	        						.success(function(data){
 	        							if(!!_this[_success]){
 	        								return _this[_success](data);	
@@ -138,12 +148,12 @@ angular.module('ngapi',[])
 	        					}
 								break;
 							case 'post':
-								var _url = apiurl+_reg.specColon(_tmpapi[0])[1];
-								$http.post(_url, {
-							        something: true,
-							        color: 'blue',
-							        one: 1
-							    }).success(function(data){
+								var _url = apiroutes+apitools.reg.specColon(_tmpapi[0])[1];
+								var _sd = {};
+								angular.forEach(_tmpdata,function(v,i,o){
+									_sd[v.split(':')[0]]=_this[v.split(':')[0]]
+								});
+								$http.post(_url,_sd).success(function(data){
 						    		if(!!_this[_success]){
         								return _this[_success](data);	
         							}else{
@@ -152,7 +162,7 @@ angular.module('ngapi',[])
 							    });
 								break;
 							case 'restfullogin':
-								var _nonce = _reg.specColon(_tmpapi[0])[1];
+								var _nonce = apitools.reg.specColon(_tmpapi[0])[1];
 								var _login = _tmpapi[1];
 								$http.get(apiroutes+_nonce).success(function (data) {
 							      	var password = apicrypto(_this.password);
@@ -163,7 +173,7 @@ angular.module('ngapi',[])
 								      	hash: hash, 
 								      	key: data.data.key
 								    }).success(function(data){
-								    	console.log(data);
+								    	//console.log(data);
 								    	if(!!_this[_success]){
 	        								return _this[_success](data);	
 	        							}else{
@@ -174,12 +184,11 @@ angular.module('ngapi',[])
 							    break;
 						};
 	        		}else{
-	        			console.log('Your youmeb value is wrong! must be=>(data)(method:apiurl)')
+	        			console.log('Your ng-api value is wrong! must be=>(data)(method:apiurl)')
 	        		};
         		});	
         	},
 		};
 	}]);
 
-		
 		
