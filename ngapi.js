09085d397df3b75aa412740267c06581eeffdefb
iamblue <tonyone0902@gmail.com>
2013-12-06 15:49:13 +0800

    update Issue #6

--- ngapi.js
+++ ngapi.js
@@ -17,7 +17,9 @@ angular.module('ngapi',[])
 			return routes;
 		};
 	}])
-	.service('apitools',function(){
+	.service('apitools',function($http){
+		this.apiroutes = {},
+		this.status = [],
 		/* validate method part , you can expand it by apivalidate*/
 		/* if false , it can't pass this api */
 		this.validate = {
@@ -55,134 +57,160 @@ angular.module('ngapi',[])
 					return false;
 				}
 			}
-		}
+		},		
+		this.api = {
+			scope : {},
+			validatedata :(function(data,_this,apivalidate){
+				this.status.length = 0;
+				var _c = this.reg.specColon; 
+				var validate = this.validate;
+				angular.forEach(data,function(v,i,o){
+					if(validate[_c(v)[1]]){
+					//Following is use apitools to validate.
+						if(!!!validate[_c(v)[1]](_this[_c(v)[0]])){
+							this.status.push(false);
+						}
+					}else{
+					//Following is use user setting to validate.
+						if(!!!apivalidate[_c(v)[1]](_this[_c(v)[0]])){
+							this.status.push(false);
+						}
+					}
+				});
+			}).bind(this),			
+			typeofapi:function(){
+				return typeof apiroutes
+			},
+			//if pass validate
+			passcheck: (function(api){
+				return this.reg.specColon(api[0])[0] || this.apiroutes[api[0].split('.')[0]][api[0].replace(api[0].split('.')[0]+'.','')].methods[0].toLowerCase();
+			}).bind(this),
+			//
+			http:(function (method,url,data,success,err){
+				var _http = $http;
+				_http[method](url,data)
+				.success(function(d,s,h,c){
+					if(!!success){
+						return success.apply(success,[d,s,h,c]);
+					}
+				})
+				.error(function(d,s,h,c){
+					if(!!err){
+						return err.apply(err,[d,s,h,c]);	
+					}
+				});
+			}).bind(this),
+			get:(function(){
+				return{
+					url:(function(type,api){
+						switch(type){
+							case 'string':
+								return this.apiroutes+this.reg.specColon(api[0])[1]
+								break;
+							case 'object':
+								return this.apiroutes[api[0].split('.')[0]].url+this.apiroutes[api[0].split('.')[0]][api[0].replace(api[0].split('.')[0]+'.','')].path;
+								break;
+						}
+					}).bind(this),
+					data:(function(){
+						//
+					}).bind(this)
+				}
+			}).bind(this),
+			sentapi:function(methods,api,data,success,err){
+				var get = this.get();
+				var _this = this.scope;
+				var $_http = this.http;
+				switch (methods){
+						case 'get':
+							if(this.typeofapi == 'string'){
+								var url = get.url('string',api);
+								var _sd = {};
+								angular.forEach(data,function(v,i,o){
+									_sd[v.split(':')[0]]=_this[v.split(':')[0]]
+								});
+								return $_http('get',url,{params: _sd});	
+							}else{
+								var url = get.url('object',api);
+								return $_http('get', url, '', _this[success], _this[err]);
+							}
+							break;
+						case 'post':
+							if (this.typeofapi == 'string'){
+								var url = get.url('string',api);
+							}else{
+								var url = get.url('object',api); 
+							}
+							var _sd = {};
+							angular.forEach(data,function(v,i,o){
+								_sd[v.split(':')[0]]=_this[v.split(':')[0]];
+							});
+							return $_http('post', url, _sd, _this[success], _this[err]);
+							break;
+						case 'restfullogin':
+							// var _nonce = this.reg.specColon(api[0])[1];
+							// var _login = api[1];
+							// $http.get(apiroutes+_nonce).success(function (data) {
+							// 	var password = apicrypto(_this.password);
+							// 	var hash = apicrypto([password, data.data.nonce, apicnonce].sort().join(''));
+							// 	return 
+							// 		$_http('post',apiroutes+_login,{
+							// 			login: _this.user,
+							// 			cnonce: apicnonce, 
+							// 			hash: hash, 
+							// 			key: data.data.key
+							// 		});
+							// });
+						break;
+					};
+			},
+			//if type has some error
+			error:{
+				typeError: function(){
+					throw new SyntaxError('Your ng-api value is wrong! must be=>(data)(method:apiurl)');
+				}
+			}
+		};
 	})
 	.directive('ngApi',['apiroutes', 'apitools', 'api', '$http',function(apiroutes, apitools, api, $http){
 		return {
 			replace: true,
 			restrict: "AE",
 			link: function (scope, elem, attr) {
-				console.log(api);
+				//Setting cross domain
+				$http.defaults.useXDomain = true;
+
 				var apicnonce = api.cnonce || '';
 				var apicrypto = api.crypto || '';
 				var apivalidate = api.validate || {};
-				
-				var _this = scope;
-				var _s = attr.ngApi;
-				_s = _s.replace(' ','');
+
+				var _s = attr.ngApi.replace(' ','');
 				var _d = /(\(.*\))(\(.*\))/; //()()
 				var _dd = /(^\(\(|^\(\w+\(.*)/; // (())
+				
 				var _data = _s.match(_d);
 
+				var s = apitools.api; 
+				apitools.apiroutes = apiroutes;
+				var _this =scope;
 				elem.bind('click',function(){
+					s.scope = _this;
 					if(!_d.test(_data[1]) && !_data[1].match(_dd) && !_data[2].match(_dd)){
-						var _tmpdata = apitools.reg.init(_data[1]);
-						var _tmpapi = apitools.reg.init(_data[2]);
-						if(!!_data[3]){
-							var _tmpvalidateerr = apitools.reg.init(_data[3]);	
-						}
-						var _ckapi = /:/;
-						var _chk = apitools.reg.specColon; 
-						var status = [];
-						angular.forEach(_tmpdata,function(v,i,o){
-							if(apitools.validate[_chk(v)[1]]){
-							//Following is use apitools to validate.
-								if(!!!apitools.validate[_chk(v)[1]](_this[_chk(v)[0]])){
-								//If this valide way exit
-									if(!!_data[3]){
-										_this[_tmpvalidateerr](_chk(v)[0]);
-									};
-									//if it not pass validation , push it to status array.
-									status.push(false);
-								}
-							}else{
-							//Following is use user setting to validate.
-								if(!!!apivalidate[_chk(v)[1]](_this[_chk(v)[0]])){
-								//If this valide way exit
-									if(!!_data[3]){
-										_this[_tmpvalidateerr](_chk(v)[0]);
-									};
-									//if it not pass validation , push it to status array.
-									status.push(false);
-								}
-							}
-						});
-						var _success = attr.apiSuccess;
-						var _error = attr.apiError;
-						var $_http = function (method,url,data){
-							$http[method](url,data)
-							.success(function(d,s,h,c){
-								if(!!_this[_success]){
-									return _this[_success].apply(_this[_success],[d,s,h,c]);
-								}
-							})
-							.error(function(d,s,h,c){
-								if(!!_this[_error]){
-									return _this[_error].apply(_this[_error],[d,s,h,c]);	
-								}
-							});
-						}
-						var passcheck =function (){
-							var _m = apitools.reg.specColon(_tmpapi[0])[0] || apiroutes[_tmpapi[0].split('.')[0]][_tmpapi[0].replace(_tmpapi[0].split('.')[0]+'.','')].methods[0];
-							_m = _m.toLowerCase();
-
-							$http.defaults.useXDomain = true;
-
-			  			switch (_m){
-								case 'get':
-									if(_ckapi.test(_tmpapi)){
-										var _sd = {};
-										angular.forEach(_tmpdata,function(v,i,o){
-											_sd[v.split(':')[0]]=_this[v.split(':')[0]]
-										});
-										return $_http('get',apiroutes+apitools.reg.specColon(_tmpapi[0])[1],{params: _sd});	
-									}else{
-										return $_http('get',apiroutes[_tmpapi[0].split('.')[0]].url+apiroutes[_tmpapi[0].split('.')[0]][_tmpapi[0].replace(_tmpapi[0].split('.')[0]+'.','')].path,'');	
-									}
-									break;
-								case 'post':
-									var _url,_sd;
-									if (typeof apiroutes == 'string'){
-										_url= apiroutes+apitools.reg.specColon(_tmpapi[0])[1];
-									}else{
-										var _api,_tmpa,_tmps;
-										_api = _tmpapi[0].split('.');
-										_tmpa = [];
-										_tmps = '';
-										for (var i = 1; i <= _api.length - 1; i++) {
-											_tmpa.push(_api[i]);
-										};
-										_tmps = _tmpa.join().replace(/\,/g,'.');
-										_url= apiroutes.routes.url+apiroutes[_api[0]][_tmps].path;
-									}
-									_sd = {};
-									angular.forEach(_tmpdata,function(v,i,o){
-										_sd[v.split(':')[0]]=_this[v.split(':')[0]];
-									});
-									return $_http('post',_url,_sd);	
-									break;
-								case 'restfullogin':
-									var _nonce = apitools.reg.specColon(_tmpapi[0])[1];
-									var _login = _tmpapi[1];
-									$http.get(apiroutes+_nonce).success(function (data) {
-										var password = apicrypto(_this.password);
-										var hash = apicrypto([password, data.data.nonce, apicnonce].sort().join(''));
-										return 
-											$http('post',apiroutes+_login,{
-												login: _this.user,
-												cnonce: apicnonce, 
-												hash: hash, 
-												key: data.data.key
-											});
-									});
-								break;
-							};
-						}// final function
+						//(data...)(api...)
+						var data = apitools.reg.init(_data[1]);//show data
+						var api = apitools.reg.init(_data[2]); //show api
+						s.validatedata(data,_this,apivalidate);
+						var methods = s.passcheck(api);
 						if (status.length==0){
-							return passcheck();
-						}
+							return s.sentapi(methods, api, data, attr.apiSuccess, attr.apiError);
+						};
 					}else{
-						throw new SyntaxError('Your ng-api value is wrong! must be=>(data)(method:apiurl)');
+						if(){
+							
+						}
+						else{
+							return s.error.typeError();
+						}
 					};
 				});	
 			},
